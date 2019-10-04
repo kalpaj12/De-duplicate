19fc7e5c8e050cac4785ddb724cbd55bb9c94bfe
kalpaj12 <kalpaj.agrawalla02@gmail.com>
2019-10-04 18:15:56 +0530

    [chore]: Added switch to nonDuplicate tab and close duplicateTab functionality
    1. Now, when the user opens a duplicate tab, the window focus changes to the window containing the nonDuplicate tab and is highlighted.

--- src/scripts/background.js
+++ src/scripts/background.js
@@ -8,6 +8,14 @@ chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
         });
     } else if (msg.action.localeCompare("deleteDuplicate") === 0) {
         chrome.tabs.remove(msg.tabid);
+    } else if (msg.action.localeCompare("switchNonDuplicate") === 0) {
+        chrome.tabs.update(msg.tabInfo.duplicateOfid, {
+            active: true
+        }, function(tab) {
+            chrome.windows.update(tab.windowId, {
+                focused: true
+            });
+        });
     }
     return true;
 });
--- src/scripts/content.js
+++ src/scripts/content.js
@@ -1,13 +1,19 @@
-var GreatSuspenderDomain = "chrome-extension://klbibkeccnjlkjkiokjodocebajanakg/suspended.html";
+var greatSuspenderDomain = "chrome-extension://klbibkeccnjlkjkiokjodocebajanakg/suspended.html";
 
-var TotalTabCount = 0;
-var DuplicateTabCount = 0;
+var totalTabCount = 0;
+var nonduplicateTabCount = 0;
+var duplicateTabCount = 0;
 
-var Tabset = new Set();
-var DuplicateTabs = new Array();
+var duplicateURLTrackerSet = new Set();
+var duplicateTabinfo = new Array();
+var nonduplicateTabinfo = new Array();
 
-function cleanDuplicates(DuplicateTabs) {
-    DuplicateTabs.forEach((element) => {
+function cleanDuplicates(duplicateTabinfo) {
+    duplicateTabinfo.forEach((element) => {
+        chrome.runtime.sendMessage({
+            action: "switchNonDuplicate",
+            tabInfo: element
+        });
         chrome.runtime.sendMessage({
             action: "deleteDuplicate",
             tabid: element.id
@@ -20,29 +26,34 @@ chrome.runtime.sendMessage({
 }, function(response) {
     response.data.forEach((element) => {
         if (element.incognito === false) {
-            if (element.url.includes(GreatSuspenderDomain)) {
+            totalTabCount++;
+            if (element.url.includes(greatSuspenderDomain)) {
                 var urlBegin = element.url.indexOf("&uri=");
                 element.url = element.url.substring(urlBegin + 5);
             }
-            Tabset.add(element.url);
-            TotalTabCount++;
-            DuplicateTabCount++;
 
-            if (DuplicateTabCount !== Tabset.size) {
+            var tabInfo = {
+                id: element.id,
+                url: element.url,
+                windowId: element.windowId,
+                duplicateOfid: element.id
+            };
 
-                var tabInfo = {
-                    id: element.id,
-                    url: element.url,
-                    windowId: element.windowId
-                };
-
-                DuplicateTabs.push(tabInfo);
-                DuplicateTabCount--;
+            if (duplicateURLTrackerSet.has(tabInfo.url) === false) {
+                nonduplicateTabinfo.push(tabInfo);
+                duplicateURLTrackerSet.add(tabInfo.url);
+                nonduplicateTabCount++;
+            } else {
+                const tab = nonduplicateTabinfo.find(({
+                    url
+                }) => url == tabInfo.url);
+                tabInfo.duplicateOfid = tab.id;
+                duplicateTabinfo.push(tabInfo);
+                duplicateTabCount++;
             }
         }
     });
-    if (TotalTabCount - Tabset.size > 0) {
-        // console.log("Duplicates: " + (TotalTabCount - Tabset.size));
-        cleanDuplicates(DuplicateTabs);
+    if (duplicateTabCount > 0) {
+        cleanDuplicates(duplicateTabinfo);
     }
 });
