4ccafd55ed5225e3591d8c792340ad00cc9e0f72
kalpaj12 <kalpaj.agrawalla02@gmail.com>
2019-10-22 18:07:12 +0530

    [chore]: Completed frontend of popup

--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -1,10 +1,47 @@
 'use strict';
 
-function openPopup() {
+var ClosedduplicateCount = document.getElementById('ClosedduplicateCount');
+chrome.storage.local.get(['Deduplicate'], function(result) {
+    if ((['Deduplicate'] in result)) {
+        var res = result.Deduplicate.totalduplicatesclosed;
+        var value = parseInt(res, 10);
+        if (value > 1000) {
+            ClosedduplicateCount.textContent = "Total Duplicates Closed: 1000+";
+        } else {
+            ClosedduplicateCount.textContent = "Total Duplicates Closed: " + value;
+        }
+    } else {
+        ClosedduplicateCount.textContent = "Total Duplicates Closed: " + 0;
+    }
+});
+
+var urlblacklist = document.getElementById('urlblacklist');
+urlblacklist.addEventListener('click', urlblacklistfn);
+
+var domainblacklist = document.getElementById('domainblacklist');
+domainblacklist.addEventListener('click', domainblacklistfn);
+
+var openPopup = document.getElementById('openPopup');
+openPopup.addEventListener('click', openPopupfn);
+
+var githubpage = document.getElementById('githubpage');
+githubpage.addEventListener('click', githubpagefn);
+
+function urlblacklistfn() {
+
+}
+
+function domainblacklistfn() {
+
+}
+
+function openPopupfn() {
     chrome.runtime.openOptionsPage();
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('openPopup').addEventListener('click',
-        openPopup);
-})
+function githubpagefn() {
+    chrome.tabs.create({
+        url: "https://github.com/kalpaj12/De-duplicate",
+        active: true
+    });
+}
