d3669931665f326dabf823f73746fbac0d7de77a
kalpaj12 <kalpaj.agrawalla02@gmail.com>
2020-01-06 20:26:13 +0530

    Frontend cleanup

--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -1,22 +1,5 @@
 'use strict';
 
-// Set totalDuplicatesClosed count
-var totalDuplicatesClosed = document.getElementById('totalDuplicatesClosed');
-chrome.storage.local.get(['Deduplicate'], function(result) {
-    if ((['Deduplicate'] in result)) {
-        var res = result.Deduplicate.totalduplicatesclosed;
-        var value = parseInt(res, 10);
-        if (value > 1000) {
-            totalDuplicatesClosed.textContent = "Total Duplicates Closed: 1000+";
-        } else {
-            totalDuplicatesClosed.textContent = "Total Duplicates Closed: " + value;
-        }
-    } else {
-        totalDuplicatesClosed.textContent = "Total Duplicates Closed: 0";
-    }
-});
-
-
 // various other options in the popup page
 var blackListCurrURL = document.getElementById('blackListCurrURL');
 blackListCurrURL.addEventListener('click', blackListCurrURLfn);
