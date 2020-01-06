b8c06003c7d4979c3eab12b64e6302ba1560e9d3
kalpaj12 <kalpaj.agrawalla02@gmail.com>
2020-01-06 13:16:38 +0530

    Addresses a lot of issues,
    P.s. Still in dev

--- src/scripts/background.js
+++ src/scripts/background.js
@@ -1,65 +1,10 @@
 'use strict';
 
-var greatSuspenderDomain = "chrome-extension://klbibkeccnjlkjkiokjodocebajanakg/suspended.html";
+var duplicatesArr = new Array();
+var nonduplicateArr = new Array();
+var nonduplicateURLSet = new Set();
+var newTabsArr = new Array();
 
-var nonduplicateTabCount = 0;
-var duplicateTabCount = 0;
-
-var duplicateURLTrackerSet = new Set();
-var duplicateTabinfo = new Array();
-var nonduplicateTabinfo = new Array();
-var newTabDetails = new Array();
-
-chrome.browserAction.setBadgeBackgroundColor({
-    color: [0, 0, 0, 255]
-});
-
-function getAllTabs(callback) {
-    chrome.tabs.query({}, function(tabs) {
-        callback(tabs);
-    });
-}
-
-function closeDuplicateTabs(tabId) {
-    chrome.tabs.get(tabId, function(tab) {
-        if (tab && tab.id) {
-            chrome.tabs.remove(tab.id);
-        }
-    });
-}
-
-function switchNonDuplicate(tabInfo) {
-    chrome.tabs.update(tabInfo.duplicateOfid, {
-        active: true
-    }, function(tab) {
-        chrome.windows.update(tab.windowId, {
-            focused: true
-        }, function() {
-            closeDuplicateTabs(tabInfo.id);
-        });
-    });
-}
-
-function removeSingleDuplicate(duplicateTabinfo, switchtononduplicate) {
-    if (switchtononduplicate)
-        switchNonDuplicate(duplicateTabinfo[0]);
-    else
-        closeDuplicateTabs(duplicateTabinfo[0].id);
-}
-
-function removeMultipleDuplicates(duplicateTabinfo) {
-    duplicateTabinfo.forEach((element) => {
-        closeDuplicateTabs(element.id);
-    });
-}
-
-function setBadgeText(text) {
-    chrome.browserAction.setBadgeText({
-        text: text
-    });
-}
-
-// Context => Right click
 const rightclickOptions = {
     "blacklist": "Blacklist options",
     "visit_github": "View project on GitHub"
@@ -76,8 +21,71 @@ chrome.contextMenus.onClicked.addListener(function(info, tab) {
     }
 });
 
-chrome.runtime.onInstalled.addListener(function() {
+const greatSuspenderURL = "chrome-extension://klbibkeccnjlkjkiokjodocebajanakg/suspended.html";
 
+function URLCleanUp(tab) {
+    if (tab.url.includes(greatSuspenderURL)) {
+        var urlBegin = tab.url.indexOf("&uri=");
+        tab.url = tab.url.substring(urlBegin + 5);
+    }
+    return tab;
+}
+
+chrome.browserAction.setBadgeBackgroundColor({
+    color: [0, 0, 0, 255]
+});
+
+function setBadgeText(text) {
+    chrome.browserAction.setBadgeText({
+        text: text
+    });
+}
+
+function getAllTabs(callback) {
+    chrome.tabs.query({}, function(tabs) {
+        callback(tabs);
+    });
+}
+
+function closeDuplicateTab(tabId) {
+    chrome.tabs.get(tabId, function(tab) {
+        if (tab && tab.id) {
+            chrome.tabs.remove(tab.id);
+        } else {
+            console.error("Tab not found!", tabId);
+        }
+    });
+}
+
+function switchFocus(tabInfo) {
+    chrome.tabs.update(tabInfo.duplicateOfid, {
+        active: true
+    }, function(tab) {
+        chrome.windows.update(tab.windowId, {
+            focused: true
+        }, function() {
+            closeDuplicateTab(tabInfo.id);
+        });
+    });
+}
+
+function removeSingleDuplicate(duplicatesArr, changefocus) {
+    if (changefocus)
+        switchFocus(duplicatesArr[0]);
+    else
+        closeDuplicateTab(duplicatesArr[0].id);
+}
+
+function removeDuplicates(duplicatesArr) {
+    duplicatesArr.forEach((tab, pos) => {
+        if (pos !== duplicatesArr.length - 1)
+            closeDuplicateTab(tab.id);
+        else
+            removeSingleDuplicate([tab], true);
+    });
+}
+
+chrome.runtime.onInstalled.addListener(function() {
     for (let key of Object.keys(rightclickOptions)) {
         chrome.contextMenus.create({
             id: key,
@@ -90,167 +98,153 @@ chrome.runtime.onInstalled.addListener(function() {
     chrome.runtime.openOptionsPage();
 
     getAllTabs(function(tabs) {
-        var chromeCleanupSynchoronous = new Promise((resolve, reject) => {
-            tabs.forEach((element, pos) => {
-                if (!element.incognito) {
-                    if (element.url.includes(greatSuspenderDomain)) {
-                        var urlBegin = element.url.indexOf("&uri=");
-                        element.url = element.url.substring(urlBegin + 5);
-                    }
+        var asyncInit = new Promise((resolve, reject) => {
+            tabs.forEach((tab) => {
+                if (!tab.incognito) {
+                    tab = URLCleanUp(tab);
 
                     var tabInfo = {
-                        id: element.id,
-                        url: element.url,
-                        windowId: element.windowId,
-                        duplicateOfid: element.id
+                        id: tab.id,
+                        url: tab.url || tab.pendingUrl,
+                        windowId: tab.windowId,
+                        duplicateOfid: tab.id
                     };
 
                     if (tabInfo.url) {
-                        if (!duplicateURLTrackerSet.has(tabInfo.url)) {
-                            duplicateURLTrackerSet.add(tabInfo.url);
-                            nonduplicateTabinfo.push(tabInfo);
-                            nonduplicateTabCount++;
+                        if (!nonduplicateURLSet.has(tabInfo.url)) {
+                            nonduplicateURLSet.add(tabInfo.url);
+                            nonduplicateArr.push(tabInfo);
                         } else {
-                            const tab = nonduplicateTabinfo.find(({
-                                url
-                            }) => url === tabInfo.url);
-                            tabInfo.duplicateOfid = tab.id;
-                            duplicateTabinfo.push(tabInfo);
-                            duplicateTabCount++;
+                            const nonduplicatetabversion = nonduplicateArr.find(({
+                                url,
+                                id
+                            }) => url === tabInfo.url && id !== tabInfo.id);
+                            tabInfo.duplicateOfid = nonduplicatetabversion.id;
+                            duplicatesArr.push(tabInfo);
                         }
+                    } else {
+                        // This should be non-reachable as tabInfo takes either tab.url || tab.pendingUrl
+                        console.error("Tab had no URL", tab);
                     }
                 }
-                if (pos === tabs.length - 1) resolve();
             });
+            resolve();
         });
 
-        chromeCleanupSynchoronous.then(() => {
-            setBadgeText(nonduplicateTabCount.toString());
-            chrome.storage.local.set({
-                Deduplicate: {
-                    totalduplicatesclosed: duplicateTabCount,
-                    nonduplicateTabinfo,
-                }
-            });
-
-            if (duplicateTabCount == 1) {
-                removeSingleDuplicate(duplicateTabinfo, false);
-            } else if (duplicateTabCount > 1) {
-                removeMultipleDuplicates(duplicateTabinfo);
+        asyncInit.then(() => {
+            setBadgeText(nonduplicateArr.length.toString());
+            if (duplicatesArr.length) {
+                removeDuplicates(duplicatesArr);
             }
         });
     });
 });
 
 chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
-    console.log("Tab is closing", tabId);
-    var tab = nonduplicateTabinfo.find(({
+    // console.log("Tab is closing", tabId);
+    var tab = nonduplicateArr.find(({
         id
     }) => id == tabId);
 
     if (tab) {
-        nonduplicateTabinfo = nonduplicateTabinfo.filter(function(t) {
-            return t.id !== tab.id;
+        nonduplicateArr = nonduplicateArr.filter(function(newTab) {
+            return newTab.id !== tab.id;
         });
-        setBadgeText(nonduplicateTabinfo.length.toString());
+        setBadgeText(nonduplicateArr.length.toString());
     } else {
-        console.log("Extension cleaned up a duplicate");
+        // console.log("Extension cleaned up a duplicate");
     }
 });
 
-chrome.windows.onRemoved.addListener(function(wId) {
-    console.log("Window is closing", wId);
-    nonduplicateTabinfo = nonduplicateTabinfo.filter(function(tab) {
-        return tab.windowId !== wId;
+chrome.windows.onRemoved.addListener(function(windowId) {
+    // console.log("Window is closing", windowId);
+    nonduplicateArr = nonduplicateArr.filter(function(tab) {
+        return tab.windowId !== windowId;
     });
-    setBadgeText(nonduplicateTabinfo.length.toString());
+    setBadgeText(nonduplicateArr.length.toString());
 });
 
 chrome.tabs.onCreated.addListener(function(tab) {
-    console.log("new tab added:", tab);
-    newTabDetails.push(tab);
+    // console.log("new tab added:", tab);
+    newTabsArr.push(tab);
 });
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    if (tab.url.includes(greatSuspenderDomain)) {
-        var urlBegin = tab.url.indexOf("&uri=");
-        tab.url = tab.url.substring(urlBegin + 5);
-    }
+    tab = URLCleanUp(tab);
     if (changeInfo.discarded && changeInfo.discarded == true) {
-        var stab = nonduplicateTabinfo.find(({
+        var nonduplicateTab = nonduplicateArr.find(({
             url
         }) => url == tab.url);
-        console.log("Discarded Tab", stab.id, tab.id);
-        stab.id = tab.id;
+        // console.log("Discarded Tab", nonduplicateTab.id, tab.id);
+        nonduplicateTab.id = tab.id;
     } else if (changeInfo.status && changeInfo.status === "complete") {
-        var statusTab = newTabDetails.find(({
+        var updatednewTab = newTabsArr.find(({
             id
         }) => id == tabId);
 
-        if (statusTab) {
-            statusTab.status = "complete";
-            statusTab.url = tab.url;
+        if (updatednewTab) {
+            updatednewTab.status = "complete";
+            updatednewTab.url = tab.url;
 
-            var statustabdupcheck = nonduplicateTabinfo.find(({
+            var duplicateExists = nonduplicateArr.find(({
                 url,
                 id
-            }) => url === statusTab.url && id !== tab.id);
+            }) => url === updatednewTab.url && id !== tab.id);
 
-            if (statustabdupcheck) {
-
-                var removestatusTab = {
-                    id: statusTab.id,
-                    url: statusTab.url,
-                    windowId: statusTab.windowId,
-                    duplicateOfid: statustabdupcheck.id
-                };
-                removeSingleDuplicate([removestatusTab], true);
-                newTabDetails = newTabDetails.filter(function(ntab) {
-                    return ntab.id !== statusTab.id
+            if (duplicateExists) {
+                removeSingleDuplicate([{
+                    id: updatednewTab.id,
+                    duplicateOfid: duplicateExists.id
+                }], true);
+                newTabsArr = newTabsArr.filter(function(tab) {
+                    return tab.id !== updatednewTab.id
                 });
             } else {
-                var stabInfo = {
-                    id: statusTab.id,
-                    url: statusTab.url,
-                    windowId: statusTab.windowId,
-                    duplicateOfid: statusTab.id
-                };
-                console.log("pushed to nonduplicateTabinfo");
-                nonduplicateTabinfo.push(stabInfo);
-                newTabDetails = newTabDetails.filter(function(ntab) {
-                    return ntab.id !== stabInfo.id
+                // console.log("pushed to nonduplicateArr");
+                nonduplicateArr.push({
+                    id: updatednewTab.id,
+                    url: updatednewTab.url,
+                    windowId: updatednewTab.windowId,
+                    duplicateOfid: updatednewTab.id
+                });
+                newTabsArr = newTabsArr.filter(function(tab) {
+                    return tab.id !== updatednewTab.id
                 });
             }
         } else {
-            console.log("Non duplicate tab updated");
-            var tabdupcheck = nonduplicateTabinfo.find(({
+            // console.log("Non duplicate tab updated");
+            var isduplicateafterupdate = nonduplicateArr.find(({
                 url,
                 id
             }) => url === tab.url && id !== tab.id);
 
-            if (tabdupcheck) {
-                nonduplicateTabinfo = nonduplicateTabinfo.filter(function(ntab) {
-                    return ntab.id !== tab.id
+            if (isduplicateafterupdate) {
+                nonduplicateArr = nonduplicateArr.filter(function(tab) {
+                    return tab.id !== tabId
                 });
-                var removethistab = {
+                removeSingleDuplicate([{
                     id: tab.id,
                     url: tab.url,
                     windowId: tab.windowId,
-                    duplicateOfid: tabdupcheck.id
-                };
-                removeSingleDuplicate([removethistab], true);
+                    duplicateOfid: isduplicateafterupdate.id
+                }], true);
             } else {
-                var thistab = nonduplicateTabinfo.find(({
+                var isExistsinnonduplicateArr = nonduplicateArr.find(({
                     id
                 }) => id === tab.id);
-                if (thistab) {
-                    thistab.url = tab.url;
-                    thistab.status = "complete";
+                if (isExistsinnonduplicateArr) {
+                    isExistsinnonduplicateArr.url = tab.url;
+                    isExistsinnonduplicateArr.status = "complete";
                 } else {
-                    console.error("The tab was either moved from another window, and then its property was changed, or some other shit happened");
+                    nonduplicateArr.push({
+                        id: tab.id,
+                        url: tab.url || tab.pendingUrl,
+                        windowId: tab.windowId,
+                        duplicateOfid: tab.id
+                    });
                 }
             }
         }
     }
-    setBadgeText(nonduplicateTabinfo.length.toString());
+    setBadgeText(nonduplicateArr.length.toString());
 });
